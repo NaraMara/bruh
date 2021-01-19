60ce453a5ce0200cdfdc77ec2a05e7109cd10dd7
NaraMara <Actuallyschegolkovmatvey@gmail.com>
2021-01-19 19:10:00 +0700

    NYA

--- Web-Frontend15/Frontend15/wwwroot/TScript/file.js
+++ Web-Frontend15/Frontend15/wwwroot/TScript/file.js
@@ -20,8 +20,13 @@ $(function () {
     var pewds = new Pewds("Pewdiepie", 100, 100, "pewds", "Leave your memes on the reddit 19 year olds");
     var phil = new Phil("Phil Swift", 100, 100, "phil", "I SAWED THAT BOAT IN HALF AND REAPAIRED IT ONLY WITH FLEX TAPE");
     var tyler = new Tyler("Tyler1", 100, 100, "tyler", "IAM TYLER MOT*****G ONE");
-    var character;
     var game;
+    var perk1;
+    var perk2;
+    var perk3;
+    var enemy1;
+    var enemy2;
+    var enemy3;
     $('.hero').click(function () {
         if ($("#Message,#SelectButton").hasClass("show") == false) {
             $("#Message,#SelectButton").addClass("show");
@@ -55,6 +60,10 @@ $(function () {
             case "pewds":
                 {
                     game = new Game(pewds);
+                    pewds.setUpPerks();
+                    perk1 = pewds.perk1;
+                    perk2 = pewds.perk2;
+                    perk3 = pewds.perk3;
                     break;
                 }
             case "tyler":
@@ -71,19 +80,112 @@ $(function () {
                 break;
         }
     });
+    $('.perk').click(function () {
+        if (game.isHeroTurn) {
+            $('.perk').removeClass('border-primary');
+            $(this).addClass("border-primary");
+            var selectedPerk = $(this).attr("Id");
+            switch (selectedPerk) {
+                case "perk1":
+                    {
+                        game.chosenPerk = game.Char.perk1;
+                    }
+                case "perk2":
+                    {
+                        game.chosenPerk = game.Char.perk2;
+                    }
+                case "perk3":
+                    {
+                        game.chosenPerk = game.Char.perk3;
+                    }
+                default:
+                    break;
+            }
+            game.isPerkChosen = true;
+        }
+    });
+    $('.Enemy').click(function () {
+        console.log("trigirred ");
+        if (game.isHeroTurn) {
+            if (game.isPerkChosen) {
+                if (game.chosenPerk.isOneDirected) {
+                }
+                else {
+                    game.chosenPerk.Execute(game.enemies);
+                    console.log("YEAS");
+                }
+            }
+        }
+    });
 });
 var Game = /** @class */ (function () {
     function Game(Char) {
         this.Char = Char;
-        this.isHeroTurn = true;
-        this.Enemies = [];
+        //-------------------------------------------------------------------
+        this._isHeroTurn = true;
+        //------------------------------------------------------------------
+        //-------------------------------------------------------------------
+        this._isPerkChosen = false;
+        //------------------------------------------------------------------
+        this._targets = [];
+        //------------------------------------------------------------------
+        this.enemies = [];
         this.StartGame();
     }
     Game.prototype.StartGame = function () {
         $('#profile').find('.card-title').html(this.Char.name);
         $('#profile').find('.card-text').html(this.Char.motto);
+        this.enemies.push(this.CreateEnemy());
+        console.log(this.enemies[0].name);
+        console.log(this.enemies[0].discription);
+        $('#enemy1').find('.card-title').html(this.enemies[0].name);
+        $('#enemy1').find('.card-text').html(this.enemies[0].discription);
+        this.ClearEnemyCards();
+    };
+    Object.defineProperty(Game.prototype, "isHeroTurn", {
+        get: function () { return this._isHeroTurn; },
+        set: function (flag) { this._isHeroTurn = flag; },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Game.prototype, "isPerkChosen", {
+        get: function () { return this._isPerkChosen; },
+        set: function (flag) {
+            this._isPerkChosen = flag;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Game.prototype, "chosenPerk", {
+        get: function () { return this._chosenPerk; },
+        set: function (move) {
+            this._chosenPerk = move;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Game.prototype, "targets", {
+        get: function () { return this._targets; },
+        set: function (t) {
+            this._targets = t;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Game.prototype.CreateEnemy = function () {
+        var enemy = new Enemy("Regular bandit", 100, 0, "Weakass bandit ");
+        return enemy;
     };
     Game.prototype.NextStep = function () {
+    };
+    Game.prototype.ClearEnemyCards = function () {
+        /*  for (let i=0; i < this.enemies.length; i++) {
+              if (this.enemies[i].isAlive)
+                  $('#enemy' + i).removeClass("d-none")
+          }
+          */
+        if (this.enemies[0].isAlive)
+            $('#enemy1').removeClass("d-none");
     };
     return Game;
 }());
@@ -92,63 +194,160 @@ var Unit = /** @class */ (function () {
         this.name = name;
         this.MaxHP = MaxHP;
         this.MaxMana = MaxMana;
+        this.isAlive = true;
         this.MoveSet = [];
         this._HP = MaxHP;
         this._mana = MaxMana;
     }
-    Object.defineProperty(Unit.prototype, "hp", {
-        get: function () {
-            return this._HP;
-        },
-        set: function (n) {
-            this._HP += n;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Unit.prototype, "mana", {
-        get: function () {
-            return this._mana;
-        },
-        set: function (n) {
-            this._mana += n;
-        },
-        enumerable: false,
-        configurable: true
-    });
+    Unit.prototype.Death = function () {
+        console.log("DEATH...not yet implimented");
+    };
+    Unit.prototype.getDamage = function (amount) {
+        if (this._HP - amount < 0)
+            this.Death();
+        else
+            this._HP -= amount;
+    };
+    Unit.prototype.getHeal = function (amount) {
+        if (this._HP + amount > this.MaxHP)
+            this._HP = this.MaxHP;
+        else
+            this._HP = this._HP + amount;
+    };
+    Unit.prototype.lostMana = function (amount) {
+        if (this._mana - amount < 0)
+            this._mana = 0;
+        else
+            this._mana = this._HP - amount;
+    };
+    Unit.prototype.getMana = function (amount) {
+        if (this._mana + amount > this.MaxMana)
+            this._mana = this.MaxMana;
+        else
+            this._mana = this._mana + amount;
+    };
     return Unit;
 }());
 var Enemy = /** @class */ (function (_super) {
     __extends(Enemy, _super);
-    function Enemy(name, MaxHP, MaxMana) {
+    function Enemy(name, MaxHP, MaxMana, discription) {
         var _this = _super.call(this, name, MaxHP, MaxMana) || this;
         _this.name = name;
         _this.MaxHP = MaxHP;
         _this.MaxMana = MaxMana;
+        _this.discription = discription;
         return _this;
     }
     return Enemy;
 }(Unit));
+var SimpleBandit = /** @class */ (function (_super) {
+    __extends(SimpleBandit, _super);
+    function SimpleBandit(name, MaxHP, MaxMana, discription) {
+        var _this = _super.call(this, name, MaxHP, MaxMana, discription) || this;
+        _this.name = name;
+        _this.MaxHP = MaxHP;
+        _this.MaxMana = MaxMana;
+        _this.discription = discription;
+        _this.move1 = new Stab("simple stab", "weakass stab ", true);
+        _this.move2 = new SimpleSelfHeal("simple self heal", "weakass heal ", true);
+        _this.MoveSet.push(_this.move1);
+        return _this;
+    }
+    return SimpleBandit;
+}(Enemy));
 var Move = /** @class */ (function () {
-    function Move(percName, description) {
+    function Move(percName, description, _isOneDirected) {
         this.percName = percName;
         this.description = description;
+        this._isOneDirected = _isOneDirected;
     }
+    Move.prototype.Execute = function (targets) {
+    };
+    Object.defineProperty(Move.prototype, "isOneDirected", {
+        get: function () { return this._isOneDirected; },
+        enumerable: false,
+        configurable: true
+    });
     return Move;
 }());
-//Пример способности
-var TridentStike = /** @class */ (function (_super) {
-    __extends(TridentStike, _super);
-    function TridentStike(percName, description) {
-        var _this = _super.call(this, percName, description) || this;
+var Stab = /** @class */ (function (_super) {
+    __extends(Stab, _super);
+    function Stab(percName, description, _isOneDirected) {
+        var _this = _super.call(this, percName, description, _isOneDirected) || this;
         _this.percName = percName;
         _this.description = description;
+        _this._isOneDirected = _isOneDirected;
+        return _this;
+    }
+    Stab.prototype.Execute = function (targets) {
+        targets[0].getDamage(20);
+    };
+    return Stab;
+}(Move));
+var SimpleSelfHeal = /** @class */ (function (_super) {
+    __extends(SimpleSelfHeal, _super);
+    function SimpleSelfHeal(percName, description, _isOneDirected) {
+        var _this = _super.call(this, percName, description, _isOneDirected) || this;
+        _this.percName = percName;
+        _this.description = description;
+        _this._isOneDirected = _isOneDirected;
+        return _this;
+    }
+    SimpleSelfHeal.prototype.Execute = function (targets) {
+        targets[0].getHeal(30);
+    };
+    return SimpleSelfHeal;
+}(Move));
+var TridentStike = /** @class */ (function (_super) {
+    __extends(TridentStike, _super);
+    function TridentStike(percName, description, _isOneDirected) {
+        var _this = _super.call(this, percName, description, _isOneDirected) || this;
+        _this.percName = percName;
+        _this.description = description;
+        _this._isOneDirected = _isOneDirected;
         return _this;
     }
     TridentStike.prototype.Execute = function (targets) {
-        targets.hp = -80;
+        for (var i = 0; i < targets.length; i++) {
+            targets[i].getDamage(80);
+        }
+    };
+    TridentStike.prototype.BackLog = function () {
+        console.log("Hero deals 80 damage ");
     };
     return TridentStike;
+}(Move));
+var SKRATTAR = /** @class */ (function (_super) {
+    __extends(SKRATTAR, _super);
+    function SKRATTAR(percName, description, _isOneDirected) {
+        var _this = _super.call(this, percName, description, _isOneDirected) || this;
+        _this.percName = percName;
+        _this.description = description;
+        _this._isOneDirected = _isOneDirected;
+        return _this;
+    }
+    SKRATTAR.prototype.Execute = function (targets) {
+        for (var i = 0; i < targets.length; i++) {
+            targets[i].getDamage(30);
+            console.log("Hero deals 80 damage " + targets[i].name);
+        }
+    };
+    return SKRATTAR;
+}(Move));
+var SKIP = /** @class */ (function (_super) {
+    __extends(SKIP, _super);
+    function SKIP(percName, description, _isOneDirected) {
+        var _this = _super.call(this, percName, description, _isOneDirected) || this;
+        _this.percName = percName;
+        _this.description = description;
+        _this._isOneDirected = _isOneDirected;
+        return _this;
+    }
+    SKIP.prototype.Execute = function (targets) {
+        targets[0].getHeal(30);
+        targets[0].getMana(30);
+    };
+    return SKIP;
 }(Move));
 var Character = /** @class */ (function (_super) {
     __extends(Character, _super);
@@ -158,6 +357,21 @@ var Character = /** @class */ (function (_super) {
         _this.motto = motto;
         return _this;
     }
+    Object.defineProperty(Character.prototype, "perk1", {
+        get: function () { return this._perk1; },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Character.prototype, "perk2", {
+        get: function () { return this._perk2; },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Character.prototype, "perk3", {
+        get: function () { return this._perk3; },
+        enumerable: false,
+        configurable: true
+    });
     Character.prototype.Greet = function () {
         console.log(this.motto);
         $('#Message').html(this.motto);
@@ -171,8 +385,19 @@ var Pewds = /** @class */ (function (_super) {
         _this.name = name;
         _this.selector = selector;
         _this.motto = motto;
+        _this._perk1 = new TridentStike("Trident strike", "deals 80 damage to one enemy", true);
+        _this._perk2 = new SKRATTAR("SKRATTAR DU!!! FLÖRAR DU!!!", "deals 30 damage to all enemies", false);
+        _this._perk3 = new SKIP("Skip turn ", "Restore some HP/mana", false);
         return _this;
     }
+    Pewds.prototype.setUpPerks = function () {
+        $("#perk1").find('.card-title').html(this._perk1.percName);
+        $("#perk1").find('.card-text').html(this._perk1.description);
+        $("#perk2").find('.card-title').html(this._perk2.percName);
+        $("#perk2").find('.card-text').html(this._perk2.description);
+        $("#perk3").find('.card-title').html(this._perk3.percName);
+        $("#perk3").find('.card-text').html(this._perk3.description);
+    };
     return Pewds;
 }(Character));
 var Phil = /** @class */ (function (_super) {
--- Web-Frontend15/Frontend15/wwwroot/TScript/file.ts
+++ Web-Frontend15/Frontend15/wwwroot/TScript/file.ts
@@ -8,9 +8,13 @@
     let pewds: Pewds = new Pewds("Pewdiepie", 100, 100, "pewds", "Leave your memes on the reddit 19 year olds")
     let phil: Phil=new Phil("Phil Swift",100,100,"phil","I SAWED THAT BOAT IN HALF AND REAPAIRED IT ONLY WITH FLEX TAPE")
     let tyler: Tyler = new Tyler("Tyler1", 100, 100, "tyler", "IAM TYLER MOT*****G ONE")
-    let character: Character;
     let game: Game;
-   
+    let perk1: Move;
+    let perk2: Move;
+    let perk3: Move;
+    let enemy1: Enemy;
+    let enemy2: Enemy;
+    let enemy3: Enemy;
 
     $('.hero').click(function () {
         if ($("#Message,#SelectButton").hasClass("show") == false)
@@ -52,6 +56,10 @@
                 {
                      
                     game = new Game(pewds);
+                    pewds.setUpPerks();
+                    perk1 = pewds.perk1
+                    perk2 = pewds.perk2
+                    perk3 = pewds.perk3
                     break;
                 }
             case "tyler":
@@ -69,11 +77,48 @@
         }
 
     });
-    
+    $('.perk').click(function () {
+        if (game.isHeroTurn) {
+            $('.perk').removeClass('border-primary')
+            $(this).addClass("border-primary");
+            let selectedPerk = $(this).attr("Id")
+            switch (selectedPerk) {
+                case "perk1":
+                    {
+                        game.chosenPerk = game.Char.perk1
+                    }
+                case "perk2":
+                    {
+                        game.chosenPerk = game.Char.perk2
+                    }
+                case "perk3":
+                    {
+                        game.chosenPerk = game.Char.perk3
+                    }
+                default:
+                    break;
+            }            
+            game.isPerkChosen = true;
 
+        }
+    })
+    $('.Enemy').click(function () {
+        console.log("trigirred ")
+        if (game.isHeroTurn) {
 
-      
+            if (game.isPerkChosen) {
+                if (game.chosenPerk.isOneDirected) {
 
+                } else {
+                    game.chosenPerk.Execute(game.enemies)
+                    console.log("YEAS")
+                }
+
+            }
+        }
+
+    })
+ 
 
 });
 
@@ -83,14 +128,57 @@ class Game {
     public StartGame(): void {
         $('#profile').find('.card-title').html(this.Char.name)
         $('#profile').find('.card-text').html(this.Char.motto)
-
+        this.enemies.push(this.CreateEnemy())
+        console.log(this.enemies[0].name)
+        console.log(this.enemies[0].discription)
+        $('#enemy1').find('.card-title').html(this.enemies[0].name)
+        $('#enemy1').find('.card-text').html(this.enemies[0].discription) 
+        this.ClearEnemyCards();
     }
-    public isHeroTurn: boolean = true;
-    public Enemies: Unit[] = [];
+    //-------------------------------------------------------------------
+    private _isHeroTurn: boolean = true;
+    public get isHeroTurn():boolean { return this._isHeroTurn }
+    public set isHeroTurn(flag:boolean) {this._isHeroTurn=flag}
+    //------------------------------------------------------------------
+    
+    //-------------------------------------------------------------------
+    private _isPerkChosen: boolean = false;
+    public set isPerkChosen(flag: boolean) {
+        this._isPerkChosen = flag;
+    }
+    public get isPerkChosen(): boolean { return this._isPerkChosen }
+    //------------------------------------------------------------------
+    private _chosenPerk: Move;
+    public set chosenPerk(move: Move) {
+        this._chosenPerk = move;
+    }
+    public get chosenPerk(): Move {return this._chosenPerk}
+    //------------------------------------------------------------------
+    private _targets: Enemy[] = [];
+    public set targets(t: Enemy[]) {
+        this._targets = t;
+    }
+    public get targets(): Enemy[] { return this._targets }
+    //------------------------------------------------------------------
+    public enemies: Enemy[] = [];
+    private CreateEnemy(): Enemy {
+        let enemy = new Enemy("Regular bandit", 100, 0,"Weakass bandit ")
+        return enemy
+    }
     public NextStep(): void {
+
          
     }
-    
+    public ClearEnemyCards(): void   {
+      /*  for (let i=0; i < this.enemies.length; i++) {
+            if (this.enemies[i].isAlive)
+                $('#enemy' + i).removeClass("d-none")
+        }
+        */
+        if (this.enemies[0].isAlive)
+            $('#enemy1').removeClass("d-none")
+         
+    }
 }
 class Unit {
     constructor(public name: string, protected MaxHP: number, protected MaxMana: number) {
@@ -98,7 +186,7 @@ class Unit {
         this._mana = MaxMana;
     }
 
-    public isAlive:boolean;
+    public isAlive=true;
     public MoveSet: Move[] = [];
     protected _HP: number;
     protected _mana: number;
@@ -117,46 +205,114 @@ class Unit {
         else this._HP = this._HP + amount;
     }
     public lostMana(amount: number): void {
-        if (this._mana + amount > this.MaxMana)
-            this._mana = this.MaxHP;
-        else this._mana= this._HP + amount;
+        if (this._mana - amount < 0)
+            this._mana = 0;
+        else this._mana= this._HP - amount;
     }
-    //public get hp(): number {
-    //    return this._HP;
-    //}
-    //public set hp(n: number) {
-    //    this._HP += n;
-    //}
-    //public get mana(): number {
-    //    return this._mana;
-    //}
-    //public set mana(n: number) {
-    //    this._mana += n;
-    //}
+    public getMana(amount: number): void {
+        if (this._mana + amount > this.MaxMana)
+            this._mana = this.MaxMana;
+        else this._mana = this._mana + amount;
+    }
+    
 
 }
 class Enemy extends Unit {
-    constructor(public name: string, protected MaxHP: number, protected MaxMana: number) {
+    constructor(public name: string, protected MaxHP: number, protected MaxMana: number, public discription : string ) {
 
         super(name, MaxHP, MaxMana);
+
+    }
+     
+}
+class SimpleBandit extends Enemy{
+    public move1: Stab = new Stab("simple stab", "weakass stab ", true);
+    public move2: SimpleSelfHeal = new SimpleSelfHeal("simple self heal", "weakass heal ", true);
+    constructor(public name: string, protected MaxHP: number, protected MaxMana: number, public discription: string) {
+        super(name, MaxHP, MaxMana,discription)
+
+        this.MoveSet.push(this.move1);
+    }
+
+}
+
+class Move {
+    constructor(public percName: string, public description: string, protected readonly _isOneDirected: boolean) { }
+    public Execute(targets: Unit[]): void {
+         
+    }
+
+    public get isOneDirected(): boolean { return this._isOneDirected } 
+}
+class Stab extends Move {
+    constructor(public percName: string, public description: string, protected readonly _isOneDirected: boolean) {
+        super(percName, description, _isOneDirected);
+    }
+    public Execute(targets: Unit[]): void {
+        targets[0].getDamage(20);
+    }
+
+
+}
+class SimpleSelfHeal extends Move {
+    constructor(public percName: string, public description: string, protected readonly _isOneDirected: boolean) {
+        super(percName, description, _isOneDirected);
+    }
+    public Execute(targets: Unit[]): void {
+        targets[0].getHeal(30);
     }
 }
-class Move {
-    constructor(public percName: string, protected description:string ) { }
+ 
+class TridentStike extends Move {
+    constructor(public percName: string, public description: string, protected readonly _isOneDirected: boolean) {
+        super(percName, description, _isOneDirected);
+    }
+    public Execute(targets: Unit[]): void {
+        for (let i = 0; i < targets.length; i++) {
+            targets[i].getDamage(80);
+        }
+    }
+    public BackLog() {
+        
+        console.log("Hero deals 80 damage ")
+    }
     
 }
-//Пример способности
-class TridentStike  extends Move {
-    constructor(public percName: string, protected description: string) {
-        super(percName, description);
+class SKRATTAR extends Move {
+    constructor(public percName: string, public description: string, protected readonly _isOneDirected: boolean) {
+        super(percName, description, _isOneDirected);
     }
-    public Execute(targets: Unit): void {
-        targets.hp=-80
+    public Execute(targets: Unit[]): void {
+        for (let i = 0; i < targets.length; i++) {
+            targets[i].getDamage(30);
+            console.log("Hero deals 80 damage " + targets[i].name)
+        }
     }
+     
+
+}
+class SKIP extends Move {
+    constructor(public percName: string, public description: string, protected readonly _isOneDirected: boolean) {
+        super(percName, description, _isOneDirected);
+    }
+    public Execute(targets: Unit[]): void {
+        targets[0].getHeal(30);
+        targets[0].getMana(30);
+    }
+
+
 }
 class Character extends Unit {
-    
-    
+
+    protected game: Game
+
+    protected _perk1: Move;
+    protected _perk2: Move;
+    protected _perk3: Move;
+    public get perk1(): Move { return this._perk1 }
+    public get perk2(): Move { return this._perk2 } 
+    public get perk3(): Move { return this._perk3 } 
+
     constructor(name: string, MaxHP: number, MaxMana: number, protected selector: string, public motto: string) {
         super(name, MaxHP, MaxMana);
    
@@ -167,8 +323,27 @@ class Character extends Unit {
     }
 }
 class Pewds extends Character {
+    
+    
     constructor(public name: string, MaxHP: number, MaxMana: number, protected selector: string, public motto: string) {
         super(name, MaxHP, MaxMana, selector, motto);
+        this._perk1 = new TridentStike("Trident strike", "deals 80 damage to one enemy", true)
+        this._perk2 = new SKRATTAR("SKRATTAR DU!!! FLÖRAR DU!!!", "deals 30 damage to all enemies", false)
+        this._perk3 = new SKIP("Skip turn ", "Restore some HP/mana", false)
+        
+    }
+
+    public setUpPerks(): void {
+
+        $("#perk1").find('.card-title').html(this._perk1.percName)
+        $("#perk1").find('.card-text').html(this._perk1.description)
+         
+        $("#perk2").find('.card-title').html(this._perk2.percName)
+        $("#perk2").find('.card-text').html(this._perk2.description)
+
+        $("#perk3").find('.card-title').html(this._perk3.percName)
+        $("#perk3").find('.card-text').html(this._perk3.description)
+
     }
     
 }
